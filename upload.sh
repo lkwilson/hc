896cea72bddac7daed033d1b4150cd70d3e9e9e3
Lucas Wilson <lkwilson96@gmail.com>
2024-03-24 20:47:00 -0600

    Build process

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+conda build .
+out_file="$(conda build . --output)"
+tar_file="$(basename "$out_file")"
+arch_path="$(dirname "$out_file")"
+arch_file="$(basename "$arch_path")"
+base_path="$(dirname "$arch_path")"
+rsync -aRP "$base_path/./$arch_file/$tar_file" ub7:conda/
+ssh ub7 '~op/bin/pull_conda'
